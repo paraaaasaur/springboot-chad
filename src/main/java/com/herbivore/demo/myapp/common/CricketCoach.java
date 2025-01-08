ed4be98c756e6c53f22287eb5c8895057b8a5f8b
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-01-08 22:50:20 +0800

    Ch2 bean scope: singleton & prototype

--- src/main/java/com/herbivore/demo/myapp/common/CricketCoach.java
+++ src/main/java/com/herbivore/demo/myapp/common/CricketCoach.java
@@ -1,10 +1,13 @@
 package com.herbivore.demo.myapp.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Lazy
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
--- src/main/java/com/herbivore/demo/myapp/rest/DemoController.java
+++ src/main/java/com/herbivore/demo/myapp/rest/DemoController.java
@@ -11,17 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     // define a private field for dependency
     private Coach myCoach;
+    private Coach myAnotherCoach;
 
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach coach) {
+    public DemoController(@Qualifier("cricketCoach") Coach coach,
+                          @Qualifier("cricketCoach") Coach anotherCoach) {
         System.out.println("[In constructor] " + getClass().getSimpleName());
         this.myCoach = coach;
+        this.myAnotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == myAnotherCoach? (i.e. singleton/prototype?) "
+               + (myCoach == myAnotherCoach)
+               + ((myCoach == myAnotherCoach)? "(singleton)" : "(prototype)");
     }
 }
