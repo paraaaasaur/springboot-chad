2a38b7a9db6fde61097772b384d629875fe94171
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-01-09 00:08:20 +0800

    Ch2 Bean lifecycle

--- src/main/java/com/herbivore/demo/myapp/common/BaseballCoach.java
+++ src/main/java/com/herbivore/demo/myapp/common/BaseballCoach.java
@@ -1,12 +1,8 @@
 package com.herbivore.demo.myapp.common;
 
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
-@Lazy
 public class BaseballCoach implements Coach {
 
     public BaseballCoach() {
--- src/main/java/com/herbivore/demo/myapp/rest/DemoController.java
+++ src/main/java/com/herbivore/demo/myapp/rest/DemoController.java
@@ -1,6 +1,8 @@
 package com.herbivore.demo.myapp.rest;
 
 import com.herbivore.demo.myapp.common.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,27 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     // define a private field for dependency
     private Coach myCoach;
-    private Coach myAnotherCoach;
-
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach coach,
-                          @Qualifier("cricketCoach") Coach anotherCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach coach) {
         System.out.println("[In constructor] " + getClass().getSimpleName());
         this.myCoach = coach;
-        this.myAnotherCoach = anotherCoach;
+    }
+
+    @PostConstruct
+    public void aparecium() {
+        System.out.println("✨ Aparecium! MagicalBean is ready for action.");
+    }
+
+    @PreDestroy
+    public void evanesco() {
+        System.out.println("✨ Evanesco! MagicalBean is vanishing gracefully.");
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
-    }
-
-    @GetMapping("/check")
-    public String check() {
-        return "Comparing beans: myCoach == myAnotherCoach? (i.e. singleton/prototype?) "
-               + (myCoach == myAnotherCoach)
-               + ((myCoach == myAnotherCoach)? "(singleton)" : "(prototype)");
     }
 }
