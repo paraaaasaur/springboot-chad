7a24629c9ae78ae4d4540e7796a9c6149a22cbd7
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-01 17:41:52 +0800

    CH3 create

---
+++ src/main/java/com/herbivore/demo/myapp/dao/StudentDAO.java
@@ -0,0 +1,12 @@
+package com.herbivore.demo.myapp.dao;
+
+import com.herbivore.demo.myapp.entity.Student;
+
+public interface StudentDAO {
+
+	void save(Student student);
+
+	void save();
+
+	void test();
+}
