4837021742da23084a58ed79a5cc36d90521f910
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-03 02:35:56 +0800

    CH4 exception handling

--- src/main/java/com/herbivore/demo/myapp/rest/StudentBaseController.java
+++ src/main/java/com/herbivore/demo/myapp/rest/StudentBaseController.java
@@ -1,11 +1,12 @@
 package com.herbivore.demo.myapp.rest;
 
 import com.herbivore.demo.myapp.entity.Student;
+import com.herbivore.demo.myapp.exception.StudentErrorResponse;
+import com.herbivore.demo.myapp.exception.StudentNotFoundException;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +35,47 @@ public class StudentBaseController {
 	// Endpoint "/students/{studentId}"
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+		if (studentId >= students.size() || studentId < 0)
+			throw new StudentNotFoundException(
+					"Student id = " + studentId +" not found ( ^)o(^ )"
+			);
+
 		return students.get(studentId);
+	}
+
+	/** Exception handler: Annotated with {@code @ExceptionHandler}
+	 * and placed under the corresponding controller
+	 **/
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(
+			StudentNotFoundException e
+	) {
+		StudentErrorResponse body = new StudentErrorResponse();
+
+		body.setStatus(HttpStatus.NOT_FOUND.value());
+		body.setMessage(e.getMessage());
+		body.setTimestamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(
+				body,
+				HttpStatus.NOT_FOUND
+		);
+	}
+
+	/** Handles Generic Exceptions that are not
+	 * catched as {@code StudentNotFoundException}
+	 * under the endpoint of this controller */
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+		StudentErrorResponse body = new StudentErrorResponse();
+
+		body.setStatus(HttpStatus.BAD_REQUEST.value());
+		body.setMessage(e.getMessage());
+		body.setTimestamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(
+				body,
+				HttpStatus.BAD_REQUEST
+		);
 	}
 }
