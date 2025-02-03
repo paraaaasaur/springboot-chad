c982c7e5f393841577755d9d06b30da65973a2b1
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-03 22:57:33 +0800

    CH4 list employees

--- src/main/java/com/herbivore/demo/myapp/rest/EmployeeRestController.java
+++ src/main/java/com/herbivore/demo/myapp/rest/EmployeeRestController.java
@@ -1,9 +1,13 @@
 package com.herbivore.demo.myapp.rest;
 
 import com.herbivore.demo.myapp.dao.EmployeeDAO;
+import com.herbivore.demo.myapp.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +18,10 @@ public class EmployeeRestController {
 	@Autowired
 	public EmployeeRestController(EmployeeDAO employeeDAO) {
 		this.employeeDAO = employeeDAO;
+	}
+
+	@GetMapping("/employees")
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
 	}
 }
