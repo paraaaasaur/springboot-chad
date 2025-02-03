70ff7cd7f5261f4d469bce2323218a2d400cb93b
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-03 22:57:33 +0800

    CH4 list employees

--- src/main/java/com/herbivore/demo/myapp/dao/EmployeeDAO.java
+++ src/main/java/com/herbivore/demo/myapp/dao/EmployeeDAO.java
@@ -1,8 +1,12 @@
 package com.herbivore.demo.myapp.dao;
 
+import com.herbivore.demo.myapp.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
 
-
+	List<Employee> findAll();
 
 	void test();
 }
--- src/main/java/com/herbivore/demo/myapp/dao/EmployeeDAOJPAImpl.java
+++ src/main/java/com/herbivore/demo/myapp/dao/EmployeeDAOJPAImpl.java
@@ -1,8 +1,11 @@
 package com.herbivore.demo.myapp.dao;
 
+import com.herbivore.demo.myapp.entity.Employee;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class EmployeeDAOJPAImpl implements EmployeeDAO {
@@ -15,6 +18,12 @@ public class EmployeeDAOJPAImpl implements EmployeeDAO {
 	}
 
 
+	@Override
+	public List<Employee> findAll() {
+		return entityManager
+				.createQuery("FROM Employee", Employee.class)
+				.getResultList();
+	}
 
 	@Override
 	public void test() {}
