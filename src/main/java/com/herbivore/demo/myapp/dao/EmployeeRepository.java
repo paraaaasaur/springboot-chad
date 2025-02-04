7a19c6123dc32117854cb1d2c678a99de176ddda
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-05 00:58:08 +0800

    CH4 DAO -> JpaRepository

---
+++ src/main/java/com/herbivore/demo/myapp/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.herbivore.demo.myapp.dao;
+
+import com.herbivore.demo.myapp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
