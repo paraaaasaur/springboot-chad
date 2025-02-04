a1424aa8cec86f8259dcd9b3fc8298739343c7ee
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-04 21:47:23 +0800

    CH4 service: insert/update + delete

--- src/main/java/com/herbivore/demo/myapp/service/EmployeeService.java
+++ src/main/java/com/herbivore/demo/myapp/service/EmployeeService.java
@@ -15,4 +15,12 @@ import java.util.List;
 public interface EmployeeService {
 
 	List<Employee> findAll();
+
+	Employee findById(int id);
+
+	Employee save(Employee employee);
+
+	void deleteById(int id);
+
+	void test();
 }
--- src/main/java/com/herbivore/demo/myapp/service/EmployeeServiceImpl.java
+++ src/main/java/com/herbivore/demo/myapp/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.herbivore.demo.myapp.dao.EmployeeDAO;
 import com.herbivore.demo.myapp.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,6 +19,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 	public List<Employee> findAll() {
 		return employeeDAO.findAll();
 	}
+
+	@Override
+	public Employee findById(int id) {
+		return employeeDAO.findById(id);
+	}
+
+	@Transactional
+	@Override
+	public Employee save(Employee employee) {
+		return employeeDAO.save(employee);
+	}
+
+	@Transactional
+	@Override
+	public void deleteById(int id) {
+		employeeDAO.deleteById(id);
+	}
+
+
+	@Override
+	public void test() {}
 
 	@Autowired
 	public void setEmployeeDAO(EmployeeDAO employeeDAO) {
