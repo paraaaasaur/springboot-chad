1d2893eae7a292d496235131e7582df78e46ec41
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-06 13:58:02 +0800

    CH4 rename resource in plural + pagination + sorting

--- src/main/java/com/herbivore/demo/myapp/dao/EmployeeRepository.java
+++ src/main/java/com/herbivore/demo/myapp/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.herbivore.demo.myapp.dao;
 
 import com.herbivore.demo.myapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members") // custom resource name
 //@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
