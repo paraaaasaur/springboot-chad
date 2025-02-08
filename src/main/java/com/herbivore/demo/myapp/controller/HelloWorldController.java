cee21dc6ebd802b736550c062dd235288b579e77
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-09 00:20:20 +0800

    CH6 data to the model

--- src/main/java/com/herbivore/demo/myapp/controller/HelloWorldController.java
+++ src/main/java/com/herbivore/demo/myapp/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.herbivore.demo.myapp.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +18,26 @@ public class HelloWorldController {
 	// Process the HTML form
 	@GetMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+
+	/**
+	 * <ol>
+	 *	 <li>Read form data</li>
+	 *	 <li>Add data to the model</li>
+	 * </ol>
+	 * key: Make use of {@code HttpServletRequest} and {@code Model}
+	 **/
+	@GetMapping("/processFormV2")
+	public String processFormV2(
+			HttpServletRequest request,
+			Model model
+	) {
+		String name = request.getParameter("studentName");
+		String message = "Greetings, " + name.toUpperCase() + "!";
+
+		model.addAttribute("message", message);
+
 		return "helloworld";
 	}
 }
