2f38c5d70e4a2b1949980eb5d7a87c8df0219b4b
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-09 00:37:21 +0800

    CH6 @RequestParam

--- src/main/java/com/herbivore/demo/myapp/controller/HelloWorldController.java
+++ src/main/java/com/herbivore/demo/myapp/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -35,6 +36,21 @@ public class HelloWorldController {
 	) {
 		String name = request.getParameter("studentName");
 		String message = "Greetings, " + name.toUpperCase() + "!";
+
+		model.addAttribute("message", message);
+
+		return "helloworld";
+	}
+
+	/** Use {@code @RequestParam} instead of
+	 * {@code HttpServletRequest}in this example
+	 **/
+	@GetMapping("/processFormV3")
+	public String processFormV3(
+			@RequestParam("studentName") String name,
+			Model model
+	) {
+		String message = "Hail, " + name.toUpperCase() + "!";
 
 		model.addAttribute("message", message);
 
