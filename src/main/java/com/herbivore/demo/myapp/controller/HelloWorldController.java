faaddbb295cfbecc708bfc2baaa21d4900d74136
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-09 00:49:40 +0800

    CH6 GET & POST

--- src/main/java/com/herbivore/demo/myapp/controller/HelloWorldController.java
+++ src/main/java/com/herbivore/demo/myapp/controller/HelloWorldController.java
@@ -4,14 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
 	// Show initial HTML form
-	@RequestMapping("/showForm")
+	@GetMapping("/showForm")
 	public String showForm() {
 		return "helloworld-form";
 	}
@@ -45,7 +45,7 @@ public class HelloWorldController {
 	/** Use {@code @RequestParam} instead of
 	 * {@code HttpServletRequest}in this example
 	 **/
-	@GetMapping("/processFormV3")
+	@PostMapping("/processFormV3")
 	public String processFormV3(
 			@RequestParam("studentName") String name,
 			Model model
