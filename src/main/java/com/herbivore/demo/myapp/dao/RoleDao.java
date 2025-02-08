a258c7966a12ec7584c7e816c6da3aec1a0083e1
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-08 17:54:48 +0800

    CH6 Spring MVC setup: helloworld

--- src/main/java/com/herbivore/demo/myapp/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.herbivore.demo.myapp.dao;
-
-import com.herbivore.demo.myapp.entity.Role;
-
-public interface RoleDao {
-
-	Role findRoleByName(String theRoleName);
-	
-}
--- src/main/java/com/herbivore/demo/myapp/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.herbivore.demo.myapp.dao;
-
-import com.herbivore.demo.myapp.entity.User;
-
-public interface UserDao {
-
-    User findByUserName(String userName);
-    
-}
