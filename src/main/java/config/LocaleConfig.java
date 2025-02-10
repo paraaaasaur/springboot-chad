64ede5c8d9b14efe62d522ff750db3eda53157f9
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-11 03:22:48 +0800

    CH6 EXTRA: Generic Error Message, i18n

---
+++ src/main/java/config/LocaleConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		return localeResolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		// Not necessary as of Java 9; just ensure proper encoding
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+}
--- src/main/resources/static/note.md
+++ src/main/resources/static/note.md
@@ -1,6 +1,6 @@
-# Generic Error Message
+# EXTRA: Generic Error Message i18n
 
-* _messages.properties_ is a special file, and Spring will recognize by this filename 
-  * ‼️ So don't name it randomly
-  * ‼️ Put it right under the _resources/_ directory
-  * 
+1. Prepare several _messages.properties_ files in different locales using resource bundles
+2. Create a `@Configuration` class for i18n method beans
+3. Send request from the client. Include `Accept-Language : ja-JP` in header 
+4. Profit ( ・∇・)
