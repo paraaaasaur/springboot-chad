9968cedc1e9dbce4327b2df1f9dfc9eaae70f88e
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-10 21:26:39 +0800

    CH6 field validation: @Min @Max
    number -> text

--- src/main/java/com/herbivore/demo/myapp/model/Customer.java
+++ src/main/java/com/herbivore/demo/myapp/model/Customer.java
@@ -1,8 +1,6 @@
 package com.herbivore.demo.myapp.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -13,34 +11,33 @@ public class Customer {
 	@Pattern(regexp = "\\p{Alpha}+", message = "alphabetical!")
 	private String lastName;
 
+	@Min(value = 0, message = "must >= 0")
+	@Max(value = 10, message = "must <= 10")
+	private int freePasses;
+
 	public Customer() {}
 
-	public Customer(String firstName, String lastName) {
+	public Customer(String firstName, String lastName, int freePasses) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.freePasses = freePasses;
 	}
 
-	public String getFirstName() {
-		return firstName;
-	}
+	public String getFirstName() {return firstName;}
+	public void setFirstName(String firstName) {this.firstName = firstName;}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+	public String getLastName() {return lastName;}
+	public void setLastName(String lastName) {this.lastName = lastName;}
 
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+	public int getFreePasses() {return freePasses;}
+	public void setFreePasses(int freePasses) {this.freePasses = freePasses;}
 
 	@Override
 	public String toString() {
 		return "Customer{" +
 			   "firstName='" + firstName + '\'' +
 			   ", lastName='" + lastName + '\'' +
+			   ", freePasses=" + freePasses +
 			   '}';
 	}
 }
--- src/main/resources/static/note.md
+++ src/main/resources/static/note.md
@@ -1,15 +1,1 @@
-# Trimming bound data with @InitBinder
-
-- Spring MVC annotation that marks a method
-- Allows you to customize the request data binding process
-- Applies to all routing methods under that controller class
-- Paired with `WebDataBinder` param to **register customizers** to tweak incoming data from `@ModelAttribute` or `@RequestParam`
-    - e.g. `StringTrimmerEditor`: used to trim leading and trailing whitespaces
-
-        ```java
-        	@InitBinder
-        	public void initBinder(WebDataBinder dataBinder) {
-        		var ste = new StringTrimmerEditor(true);
-        		dataBinder.registerCustomEditor(String.class, ste);
-        	}
-        ```
+# @Min @Max
