d4a7e2ab633ba9eea3a95152bc58cf006854d297
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-12 13:19:48 +0800

    CH7 basic GET /employees/list

---
+++ src/main/java/com/herbivore/demo/myapp/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.herbivore.demo.myapp.controller;
+
+import com.herbivore.demo.myapp.entity.Employee;
+import com.herbivore.demo.myapp.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	// @Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@GetMapping("/list")
+	public String listEmployees(Model model) {
+		List<Employee> employees = employeeService.findAll();
+		model.addAttribute("employees", employees);
+
+		return "list-employees";
+	}
+}
