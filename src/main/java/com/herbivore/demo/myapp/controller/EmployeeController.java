7d6d5376bc6a176308aecb53056a716bc5b0859d
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-12 15:29:19 +0800

    CH7 Rafactor: (1)Bootstrap (2)static/index.html

--- src/main/java/com/herbivore/demo/myapp/controller/EmployeeController.java
+++ src/main/java/com/herbivore/demo/myapp/controller/EmployeeController.java
@@ -2,10 +2,12 @@ package com.herbivore.demo.myapp.controller;
 
 import com.herbivore.demo.myapp.entity.Employee;
 import com.herbivore.demo.myapp.service.EmployeeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -21,6 +23,7 @@ public class EmployeeController {
 	}
 
 	@GetMapping("/list")
+	@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
 	public String listEmployees(Model model) {
 		List<Employee> employees = employeeService.findAll();
 		model.addAttribute("employees", employees);
