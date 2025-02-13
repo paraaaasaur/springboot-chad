38a6a0fede1ff44efaeeac88fd2f33181061a557
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-13 17:38:59 +0800

    CH8 custom logic page
    update
    
    update

--- src/main/java/com/herbivore/demo/myapp/security/DemoSecurityConfig.java
+++ src/main/java/com/herbivore/demo/myapp/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.herbivore.demo.myapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +33,23 @@ public class DemoSecurityConfig {
 				.build();
 
 		return new InMemoryUserDetailsManager(akutan, takuan, toyota);
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(configurer ->
+			configurer.anyRequest().authenticated()
+		);
+
+		http.formLogin(form -> form
+				// GET /showMyLoginPage
+				.loginPage("/showMyLoginPage")
+				// POST /authenticateTheUser: No need to implement
+				// manually - Spring Boot offers default implementation
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll());
+
+		return http.build();
 	}
 }
