bcd6e4713f4d234a7e9680a6a086457943e3e74d
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-13 23:53:13 +0800

    CH8 logout

--- src/main/java/com/herbivore/demo/myapp/security/DemoSecurityConfig.java
+++ src/main/java/com/herbivore/demo/myapp/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.herbivore.demo.myapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -49,6 +50,9 @@ public class DemoSecurityConfig {
 				// manually - Spring Boot offers default implementation
 				.loginProcessingUrl("/authenticateTheUser")
 				.permitAll());
+
+		// logout support
+		http.logout(LogoutConfigurer::permitAll);
 
 		return http.build();
 	}
