d5457efab212f152241adefeb8cb6d52e1b4eab4
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-15 21:41:14 +0800

    CH9 @OneToOne uni CREATE: fill the //TODO!

---
+++ src/main/java/com/herbivore/demo/myapp/dao/AppDAOImpl.java
@@ -0,0 +1,26 @@
+package com.herbivore.demo.myapp.dao;
+
+import com.herbivore.demo.myapp.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+	// Hibernate-specific persistence context. Supports second-level caching.
+//	private Session session;
+	// JPA persistence context, a more general one
+	private EntityManager entityManager;
+
+	public AppDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Transactional
+	@Override
+	public void save(Instructor instructor) {
+//		session.persist(instructor);
+		entityManager.persist(instructor);
+	}
+}
