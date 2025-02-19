94d1842f2e27b4334ad7c891756f0a959855f318
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-02-19 22:35:19 +0800

    CH9 bi-1-1 with CRUD + big mass

--- src/main/java/com/herbivore/demo/myapp/MyappApplication.java
+++ src/main/java/com/herbivore/demo/myapp/MyappApplication.java
@@ -1,6 +1,9 @@
 package com.herbivore.demo.myapp;
 
+
 import com.herbivore.demo.myapp.dao.AppDAO;
+import com.herbivore.demo.myapp.entity.Instructor;
+import com.herbivore.demo.myapp.entity.InstructorDetail;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import static com.herbivore.demo.myapp.util.Trivial.aqtn;
 import static io.github.paraaaasaur.util.Toolbox.*;
 
 
@@ -28,31 +32,94 @@ public class MyappApplication {
 	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 	public CommandLineRunner commandLineRunner(String[] args) {
 		return runner -> {
-//			createInstructor();
+			createInstructor(appDAO);
 
-//			findInstructor();
+//			findInstructor(appDAO, 3);
 
-//			updateInstructor();
+//			updateInstructor(appDAO);
 
-//			deleteInstructor();
+//			deleteInstructor(appDAO, 27);
+
+//			findDetail(appDAO, 27);
+
+//			updateDetail(appDAO, 27);
+
+//			deleteDetail(appDAO, 31);
 		};
 	}
 
 	private void createInstructor(AppDAO appDAO) {
-		// TODO
+		System.out.println(cyan("> Creating Instructor (and details)"));
+
+		Instructor instructor = new Instructor("Chris", "Abroad", "aij@gmail.com");
+		InstructorDetail instructorDetail = new InstructorDetail("https://www.youtube.com/@AbroadInJapan", "Filming");
+		instructor.associate(instructorDetail);
+		appDAO.saveInstructor(instructor);
+
+		aqtn();
 	}
 
 	private void findInstructor(AppDAO appDAO, int id) {
-		// TODO
+		System.out.println(cyan("> Retrieving Instructor with id = " + id + "..."));
+		Instructor found = appDAO.findInstructorById(id);
+		if (found == null) {
+			System.out.println(yellow("> No instructor with id = " + id + " is found!"));
+			return;
+		}
+		System.out.println(found);
+		System.out.println(found.getInstructorDetail());
+
+		aqtn();
 	}
 
-	private void updateInstructor() {
-		// TODO
+	private void updateInstructor(AppDAO appDAO) {
+		System.out.println(cyan("> Updating(Trolling) instructor with id = 1..."));
+
+		Instructor instructor = appDAO.findInstructorById(1);
+		instructor.setFirstName("Foo");
+		instructor.setLastName("Bar");
+		instructor.setEmail("excellent@proton.me");
+		InstructorDetail instructorDetail = instructor.getInstructorDetail();
+		instructorDetail.setHobby("YEET");
+		instructorDetail.setYoutubeChannel("https://youtube.com/@RamenIsLife");
+
+		appDAO.updateInstructor(instructor);
+		aqtn();
 	}
 
 	private void deleteInstructor(AppDAO appDAO, int id) {
-		// TODO
+		System.out.println(cyan("> Deleting instructor + details of id = " + id));
+
+		appDAO.deleteInstructorById(id);
+		aqtn();
 	}
+
+	private void findDetail(AppDAO appDAO, int id) {
+		System.out.println(cyan("> Retrieving InstructorDetail with id = " + id + "..."));
+		InstructorDetail foundDetail = appDAO.findInstructorDetailById(id);
+		System.out.println(foundDetail);
+
+		aqtn();
+	}
+
+	private void updateDetail(AppDAO appDAO, int id) {
+		InstructorDetail detail = appDAO.findInstructorDetailById(id);
+		detail.setHobby("jumping-jack");
+		appDAO.updateInstructorDetail(detail);
+
+		aqtn();
+	}
+
+	private void deleteDetail(AppDAO appDAO, int id) {
+		System.out.println(cyan("> Deleting instructor + details of id = " + id));
+
+		appDAO.deleteInstructorDetailById(id);
+		aqtn();
+	}
+
+
+
+
 
 	@PostConstruct
 	public void aparecium() {
--- src/main/java/com/herbivore/demo/myapp/dao/AppDAO.java
+++ src/main/java/com/herbivore/demo/myapp/dao/AppDAO.java
@@ -1,14 +1,23 @@
 package com.herbivore.demo.myapp.dao;
 
 import com.herbivore.demo.myapp.entity.Instructor;
+import com.herbivore.demo.myapp.entity.InstructorDetail;
 
 public interface AppDAO {
 
-	void save(Instructor instructor);
+	void saveInstructor(Instructor instructor);
 
 	Instructor findInstructorById(int id);
 
 	void updateInstructor(Instructor instructor);
 
 	void deleteInstructorById(int id);
+
+//	void saveInstructorDetail(InstructorDetail instructorDetail);
+
+	InstructorDetail findInstructorDetailById(int id);
+
+	void updateInstructorDetail(InstructorDetail instructorDetail);
+
+	void deleteInstructorDetailById(int id);
 }
